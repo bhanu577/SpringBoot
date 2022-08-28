dd674fa45edc34538744225ce86e5527c0ef7c9a
bhanu577 <dammalapatibhanu@gmail.com>
2022-08-28 20:57:47 +0530

    Initial Commit

---
+++ src/com/code/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coach	theCoach = new BaseballCoach();
+		//create the object
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}

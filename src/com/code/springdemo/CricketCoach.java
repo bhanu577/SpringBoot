a7a19dd1cdc3182b22a12e6f70d1d481188fa583
bhanu577 <dammalapatibhanu@gmail.com>
2022-09-01 07:32:43 +0530

    Add Injection of Literal Values

--- src/com/code/springdemo/CricketCoach.java
+++ src/com/code/springdemo/CricketCoach.java
@@ -4,14 +4,38 @@ public class CricketCoach implements Coach {
 	 
 	private FortuneService fortuneService;
 	
+	// add new fields for emailaddress and team
+	
+	private String emailAddress;
+	private String team;
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAdress) {
+		System.out.println("I am inside Cricket Coach class of setter method for setEmailAddress");
+		this.emailAddress = emailAdress;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+	
+	public void setTeam(String team) {
+		System.out.println("I am inside Cricket Coach class of setter method for setTeam");
+		this.team = team;
+	}
+	
 	public CricketCoach() {
 		System.out.println("I am inside Cricket Coach class");
 	}
+	
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("I am inside Cricket Coach class of setter method");
+		System.out.println("I am inside Cricket Coach class of setter method for fortuneService");
 		this.fortuneService = fortuneService;
 	}
-
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Pratice Defency shots for 1 hr every day";
--- src/com/code/springdemo/SetterDemoApp.java
+++ src/com/code/springdemo/SetterDemoApp.java
@@ -13,6 +13,8 @@ public class SetterDemoApp {
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		// close context
 		context.close();
 	}
